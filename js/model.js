b1297a04510cf99903974753cc9d9f5d891376a2
Jon Mason <jon.mason@medsci.ox.ac.uk>
2015-03-25 10:46:18 +0000

    Modified license details
    Added contributor and website info.

--- js/model.js
+++ js/model.js
@@ -1,19 +1,24 @@
 /*
     Simulation of the Urinary Concentrating Mechanism in the Nephron (JS)
+    Idea and Science: Dr Robert Wilkins, Department of Physiology, Anatomy and Genetics, University of Oxford
+    Code: Dr Damion Young and Jon Mason, Medical Sciences Division Learning Technologies, University of Oxford (msdlt@medsci.ox.ac.uk)
+	Project Home Page: https://github.com/jonmase/nephron
     Copyright (C) 2015 University of Oxford
+	
+	This file is part of Nephron
 
-    This program is free software: you can redistribute it and/or modify
+    Nephron is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    Nephron is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with Nephron.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 //Declare stepping/running values
